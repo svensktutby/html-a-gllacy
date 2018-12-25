dc83a76fbf4ad46ae46bbf36f4066990a38a3783
svensktutby <svensk.tut.by@gmail.com>
2018-12-25 20:58:24 +0300

    Corrected layout styles

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-window.onload = function () {
-  var btnFeedback = document.getElementById('btn-feedback');
-  var modalFeedback = document.querySelector('.modal-feedback');
-  var modalOverlay = document.querySelector('.modal-overlay');
-  var modalFeedbackClose = document.querySelector('.modal-close');
+var btnFeedback = document.querySelector('.contacts__btn');
+var modalFeedback = document.querySelector('.modal-feedback');
+var modalOverlay = document.querySelector('.modal-overlay');
+var modalFeedbackClose = document.querySelector('.modal-close');
+var userName = modalFeedback.querySelector('#user-name-feedback');
 
-  btnFeedback.addEventListener('click', function (e) {
-    e.preventDefault();
-    if(!modalFeedback.classList.contains('modal-feedback--show')) {
-      modalFeedback.classList.add('modal-feedback--show');
-      modalOverlay.classList.add('modal-overlay--show');
-    }
-  });
+btnFeedback.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if(!modalFeedback.classList.contains('modal-feedback--show')) {
+    modalFeedback.classList.add('modal-feedback--show');
+    modalOverlay.classList.add('modal-overlay--show');
+    userName.focus();
+  }
+});
 
-  modalFeedbackClose.addEventListener('click', function () {
-    if(modalFeedback.classList.contains('modal-feedback--show')) {
-      modalFeedback.classList.remove('modal-feedback--show');
-      modalOverlay.classList.remove('modal-overlay--show');
-    }
-  });
-};
+modalFeedbackClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if(modalFeedback.classList.contains('modal-feedback--show')) {
+    modalFeedback.classList.remove('modal-feedback--show');
+    modalOverlay.classList.remove('modal-overlay--show');
+  }
+});
+
+modalOverlay.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if(modalFeedback.classList.contains('modal-feedback--show')) {
+    modalFeedback.classList.remove('modal-feedback--show');
+    modalOverlay.classList.remove('modal-overlay--show');
+  }
+});
