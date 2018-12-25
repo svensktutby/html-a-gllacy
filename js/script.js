6498e0558f15eba5a9c3a6033791da12837941fc
svensktutby <svensk.tut.by@gmail.com>
2018-12-26 00:03:31 +0300

    Corrected script and added animation for Modal feedback

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
-var btnFeedback = document.querySelector('.contacts__btn');
-var modalFeedback = document.querySelector('.modal-feedback');
-var modalOverlay = document.querySelector('.modal-overlay');
-var modalFeedbackClose = document.querySelector('.modal-close');
-var userName = modalFeedback.querySelector('#user-name-feedback');
+(function () {
+  /**************** Promo slider ***************/
 
-btnFeedback.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  if(!modalFeedback.classList.contains('modal-feedback--show')) {
-    modalFeedback.classList.add('modal-feedback--show');
-    modalOverlay.classList.add('modal-overlay--show');
-    userName.focus();
-  }
-});
+  /**************** Modal contacts ***************/
+  var openFeedback = document.querySelector('.contacts__btn');
+  var modalFeedback = document.querySelector('.modal-feedback');
+  var closeFeedback = modalFeedback.querySelector('.modal-close');
+  var modalOverlay = document.querySelector('.modal-overlay');
 
-modalFeedbackClose.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  if(modalFeedback.classList.contains('modal-feedback--show')) {
-    modalFeedback.classList.remove('modal-feedback--show');
-    modalOverlay.classList.remove('modal-overlay--show');
-  }
-});
+  if (modalFeedback) {
+    var formFeedback = modalFeedback.querySelector('.modal-feedback__form');
+    var userNameFeedback = formFeedback.querySelector('#user-name-feedback');
+    var emailFeedback = formFeedback.querySelector('#email-feedback');
+    var messageFeedback = formFeedback.querySelector('#message-feedback');
 
-modalOverlay.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  if(modalFeedback.classList.contains('modal-feedback--show')) {
-    modalFeedback.classList.remove('modal-feedback--show');
-    modalOverlay.classList.remove('modal-overlay--show');
+    openFeedback.addEventListener('click', function (event) {
+      openFeedbackWindow();
+    });
+
+    closeFeedback.addEventListener('click', function (event) {
+      closeFeedbackWindow();
+    });
+
+    modalOverlay.addEventListener('click', function (event) {
+      closeFeedbackWindow();
+    });
+
+    window.addEventListener('keydown', function (event) {
+      if (event.keyCode === 27) {
+        closeFeedbackWindow();
+      }
+    });
+  };
+  
+  function openFeedbackWindow() {
+    event.preventDefault();
+    if (!modalFeedback.classList.contains('modal-feedback--show') && !modalOverlay.classList.contains('modal-overlay--show')) {
+      modalFeedback.classList.add('modal-feedback--show');
+      modalOverlay.classList.add('modal-overlay--show');
+      userNameFeedback.focus();
+    }
   }
-});
+  function closeFeedbackWindow() {
+    event.preventDefault();
+    if (modalFeedback.classList.contains('modal-feedback--show') && modalOverlay.classList.contains('modal-overlay--show')) {
+      modalFeedback.classList.remove('modal-feedback--show');
+      modalOverlay.classList.remove('modal-overlay--show');
+    }
+  }
+})();
